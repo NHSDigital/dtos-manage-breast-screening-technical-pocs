46ac385ba124a35d6fc7688d8925effbb9d04696
Graham Pengelly <graham@three-tier.com>
2024-11-29 13:15:59 +0000

    Add JWTEncoder
    Class to generate a simple signed JWT containing id, firstname,
    lastname.

---
+++ src/jwt_encoder.py
@@ -0,0 +1,19 @@
+import jwt
+import os
+from datetime import datetime, timedelta
+
+
+class JWTEncoder:
+    def __init__(self, private_key: str = os.getenv("JWT_PRIVATE_KEY")):
+        self.private_key = private_key
+
+    def generate_token(self, user_id: int, first_name: str, last_name: str,
+                       expiration_minutes: int = 60) -> str:
+        payload = {
+            "id": user_id,
+            "first_name": first_name,
+            "last_name": last_name,
+            "exp": datetime.utcnow() + timedelta(minutes=expiration_minutes)
+        }
+        token = jwt.encode(payload, self.private_key, algorithm="RS256")
+        return token
