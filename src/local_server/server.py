90be3db9bb97b4affbbe1a759025a54cc51f8d71
Graham Pengelly <graham@three-tier.com>
2024-11-29 17:15:44 +0000

    Send signed JWT
    Create a signed JWT and render it to the page from 'web_server'. This
    can then be sent to the gateway (local_server) via the browser extension and the
    signature verified to prevent unauthorised messages.
    
    Signature verification will be done in a subsequent commit.
    
    Reorganise the directory structure.

--- src/local_server/server.py
+++ src/local_server/server.py
@@ -8,15 +8,24 @@ class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
             self.path = '/index.htm'
         return super().do_GET()
 
+    def do_POST(self):
+        if self.path == '/do':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            print(post_data)
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'POST request for ' + self.path.encode())
 
-def run_server(port=8080, directory='.'):
+
+def run_server(port=9090, directory='.'):
     os.chdir(directory)  # Change the working directory to serve files from
     server_address = ('', port)
     httpd = HTTPServer(server_address, CustomHTTPRequestHandler)
-    print(f"Serving on port {port}. Visit http://manage-breast-screening:{port}/")
+    print(f"Serving on port {port}. Visit http://screening-gateway:{port}/")
     httpd.serve_forever()
 
 
 if __name__ == "__main__":
     # Set the directory containing 'index.htm' and the port
-    run_server(port=8080, directory='.')
+    run_server(port=9090, directory='.')
