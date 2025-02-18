97b2d256cb0b8dc9b8d77ce80e480daf9f25b8d6
Graham Pengelly <graham@three-tier.com>
2025-02-18 17:06:06 +0000

    Participants and table component
    * Display the participant list with a simple table macro

--- poc_four/manage_screening/participant/views.py
+++ poc_four/manage_screening/participant/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Details
 
 def index(request):
-    return render(request, 'participant/index.jinja')
+    details = Details.objects.all()
+
+    headers = ["NHS number", "First name", "Last name", "Date of birth",]
+    rows = [[detail.nhs_number, detail.first_name, detail.last_name, detail.date_of_birth] for detail in details]
+    return render(request, "participant/index.jinja",
+                  {"headers": headers, "rows": rows})
