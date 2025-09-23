8dc34ec2d310da9627492280fdf392657070f68a
Graham Pengelly <graham@three-tier.com>
2025-09-23 18:02:44 +0100

    Remove the gateway HTTP endpoints
    Remove the get and confirmation endpoints as we're going to use the
    relay socket connection for that.

--- poc_five/manage_screening/gateway/urls.py
+++ poc_five/manage_screening/gateway/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import create, get, confirm
+from .views import create
 
 urlpatterns = [
     path("gateway-messages/screening-order/", create, name="create_screening_order_gateway"),
-    path("gateway-messages/<uuid:gateway_id>/", get, name="get_gateway_messages"),
-    path("gateway-messages/<uuid:gateway_id>/confirmations", confirm, name="confirm_gateway_message")
 ]
